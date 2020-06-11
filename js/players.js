4c0ad89224e9b44a8f0f140a5d524a170997c961
Himjune <himjune@mail.ru>
2020-06-11 22:08:00 +0300

    add yt

--- js/players.js
+++ js/players.js
@@ -27,18 +27,38 @@ tw_player.addEventListener(Twitch.Player.PAUSE, () => {
     console.log('PAUSE');
 });
 
+var yt_player;
+function onYouTubeIframeAPIReady() {
+  let videoId = '8Q_DG_M6vLk';
 
-var embed = new Twitch.Embed("ytContainer", {
-    width: 854,
-    height: 480,
-    channel: "pavelbratok",
-    layout: "video",
-    autoplay: true,
-    // only needed if your site is also embedded on embed.example.com and othersite.example.com 
-    parent: ["himjune.github.io"]
+  yt_player = new YT.Player('ytContainer', {
+    height: '360',
+    width: '640',
+    playerVars: { 'autoplay': 1, 'controls': 0, 'playsinline': 1 },
+    videoId: videoId,
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
   });
-
-  embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
-    var player = embed.getPlayer();
-    player.play();
-  });
+}
+function onPlayerReady(event) {
+    yt_player.setVolume(10);
+    yt_player.playVideo();
+    console.log('GOgoGo');
+}
+function onPlayerStateChange(event) {
+    // OnPlay
+    if (event.data == 1) {
+      play_spd_def = 3;
+      if (yt_player.getPlaybackRate() !== 1) yt_player.setPlaybackRate(1);
+      console.log('onP f_ts:', fixed_end_time_ts, 'f:', fixed_end_time, 'ce:', cur_end_time, 'yt:', yt_player.getCurrentTime());
+      if (!is_replay && 
+          (fixed_end_time_ts === null || 
+            (yt_player.getCurrentTime() > ((fixed_end_time - fixed_end_time_ts) + Date.now() / 1000))
+          )
+        ) {
+        save_fix_end_time();
+      }
+    }
+  }
