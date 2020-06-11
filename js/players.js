b5ac980ee98cca38587d0bd0db7ab55a1d9e24b1
Himjune <himjune@mail.ru>
2020-06-11 20:32:32 +0300

    test tw events

--- js/players.js
+++ js/players.js
@@ -1,6 +1,6 @@
 const tw_def_props = {
-    width: 320,
-    height: 180,
+    width: 400,
+    height: 300,
     channel: "fitzyhere",
     //channel: "himukee",
     layout: "video",
@@ -15,4 +15,14 @@ tw_player.addEventListener(Twitch.Player.READY, () => {
     tw_player.setVolume(1.0);
     tw_player.setMuted(false);
     tw_player.play();
+    console.log('readyEvent');
+});
+tw_player.addEventListener(Twitch.Player.PLAYING, () => {
+    console.log('playingEvent');
+});
+tw_player.addEventListener(Twitch.Player.PLAYBACK_BLOCKED, () => {
+    console.log('PLAYBACK_BLOCKED');
+});
+tw_player.addEventListener(Twitch.Player.PAUSE, () => {
+    console.log('PAUSE');
 });
