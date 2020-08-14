80e6a99b78ff9e9f3b877de852eec9dc91b28a42
Himjune <himjune@mail.ru>
2020-08-15 00:48:20 +0300

    add targetDiff stats

--- js/players.js
+++ js/players.js
@@ -321,7 +321,7 @@ function watchDog() {
         }
     }
 
-    
+
     /*
         Stats display
     */
@@ -338,4 +338,5 @@ function watchDog() {
     document.querySelector('#statsCYtF').innerText = timingStats.ytPlayerTimeFixed;
     document.querySelector('#statsCYtR').innerText = tsString(timingStats.ytPlayerTime-timingStats.ytPlayerTimeFixed+timingStats.ytStartTS);
 
+    document.querySelector('#statsCYtT').innerText = yt_player.getPlaybackRate() + '|' + timingStats.ytTarget + '/' + targetDiff + '{'+ (timingStats.ytPlayerTime - timingStats.ytTarget)+'}';
 }
